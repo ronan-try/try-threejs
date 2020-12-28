b52b04a79932330f9983ebf0bda5c9aae845474e
Ronan <liruonan@hotmail.com>
2020-12-29 00:20:44 +0800

    update: gflt

--- src/index.js
+++ src/index.js
@@ -1,34 +1,57 @@
 import * as THREE from 'three';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-// https://threejs.org/docs/index.html#manual/zh/introduction/Drawing-lines
-/** 画线 drawing lines */
+/**
+ * 创建文字
+ * https://threejs.org/docs/index.html#manual/zh/introduction/Creating-text
+ */
+
+{
+  const div = document.createElement('div');
+  div.id = 'info';
+  div.textContent = 'Description';
+  Object.assign(div.style, {
+    position: 'absolute',
+    top: '10px',
+    width: '100%',
+    textAlign: 'center',
+    zIndex: 100,
+    display: 'block'
+  });
+  document.body.appendChild(div);
+}
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xcfcfcf);
+
+const camera = new THREE.PerspectiveCamera(
+  75, window.innerWidth / window.innerHeight, 0.1, 1000
+);
+camera.position.set(0, 100, 150);
+
+const ambient = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambient);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(
-  45, window.innerWidth / window.innerHeight, 1, 500
-);
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
 
-const scene = new THREE.Scene();
+const loader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/examples/js/libs/draco/');
+loader.setDRACOLoader(dracoLoader);
 
-// 定义线条材质：
-// LineBasicMaterail / LineDashedMaterial
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-
-// 需要一个带有顶点的geometry（几何体)
-const points = [];
-points.push(new THREE.Vector3(-10, 0, 0));
-points.push(new THREE.Vector3(0, 10, 0));
-points.push(new THREE.Vector3(10, 0, 0));
-
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-const line = new THREE.Line(geometry, material);
-
-// 渲染
-scene.add(line);
-renderer.render(scene, camera);
+loader.load(
+  './gltf/pony_cartoon/scene.gltf',
+  gltf => {
+    scene.add(gltf.scene);
+  },
+  xhr => {
+    console.log(xhr.loaded / xhr.total)
+  },
+  error => {
+    console.log('110');
+    console.error(error);
+  })
